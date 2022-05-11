ab8367f161953516f14e96c801d20224e8c2fd63
bjahnke <bjahnke71@gmail.com>
2022-05-11 17:27:07 -0400

    added jupyter files to improve organization

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -5,6 +5,37 @@ import regime
 import yfinance
 import pandas as pd
 import json
+import pickle
+import data_manager.utils as sbtu
+
+
+def load_data_package():
+    _data_loader = sbtu.DataLoader.init_from_paths(r'..\data_args\other.json', r'..\data_args\base.json')
+    _strategy_path = _data_loader.file_path('strategy_lookup.pkl')
+    with open(_strategy_path, 'rb') as f:
+        _strategy_lookup = pickle.load(f)
+
+    _entry_path = _data_loader.file_path('entry_table.pkl')
+    with open(_entry_path, 'rb') as f:
+        _entry_table = pickle.load(f)
+
+    _peak_path = _data_loader.file_path('peak_table.pkl')
+    with open(_peak_path, 'rb') as f:
+        _peak_table = pickle.load(f)
+
+    _bench_str = 'SPY'
+    _interval = '15m'
+    _price_data = pd.read_csv(_data_loader.history_path(), index_col=0, header=[0, 1]).astype('float64')
+    _bench = pd.read_csv(_data_loader.bench_path(), index_col=0).astype('float64')
+    _strategy_overview = pd.read_csv(_data_loader.file_path('stat_overview.csv'))
+    return (
+        _price_data,
+        _bench,
+        _strategy_overview,
+        _peak_table,
+        _entry_table,
+        _strategy_lookup
+    )
 
 
 def load_cbpro_data(other_path, base_path):
