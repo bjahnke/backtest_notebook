fb8d1957c996b8116a8b8b886dd5e233950bb9e0
bjahnke <bjahnke71@gmail.com>
2022-05-11 10:32:05 -0400

    updated to test out crypto

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,14 +1,28 @@
 import data_manager.utils
+import data_manager.scanner as scanner
 import src.floor_ceiling_regime
 import regime
-
+import yfinance
+import pandas as pd
 import json
+
+
+def load_cbpro_data(other_path, base_path):
+    data_loader = data_manager.utils.DataLoader.init_from_paths(other_path, base_path)
+    price_data = pd.read_csv(data_loader.history_path(), index_col=0, header=[0, 1])
+    price_data = price_data.ffill()
+    ticks = list(price_data.columns.levels[0].unique())
+    return ticks, data_manager.utils.PriceGlob(price_data), None, None, data_loader
+
 
 if __name__ == '__main__':
     with open(r'..\data_args\scan_args.json', 'r') as args_fp:
         _args = json.load(args_fp)
+    load_data = _args['load_data']
+
     data_manager.utils.main(
         scan_args=_args,
         strategy_simulator=src.floor_ceiling_regime.fc_scale_strategy,
-        expected_exceptions=(regime.NotEnoughDataError, src.floor_ceiling_regime.NoEntriesError)
+        expected_exceptions=(regime.NotEnoughDataError, src.floor_ceiling_regime.NoEntriesError),
+        scan_data=load_cbpro_data(load_data['other_path'], load_data['base_path'],)
     )
--- scripts/cbpro_sandbox.py
+++ scripts/cbpro_sandbox.py
@@ -1,13 +1,40 @@
 import cbpro_access
 import json
-
 import pandas as pd
+import data_manager.utils as dmu
+import data_manager.scanner as scanner
 
-if __name__ == '__main__':
+
+def download_macro():
+    interval = 1
+    interval_type = 'h'
+    base_address = fr'C:\Users\bjahnke\OneDrive - bjahnke\OneDrive\algo_data\history\cbpro_{interval}{interval_type}'
+    num_bars = 600
+
+    interval_str = f'{interval}{interval_type}'
+
     with open(r'..\credentials\cbpro.json', 'r') as cred_file:
         cb_creds = json.load(cred_file)
     client = cbpro_access.CbproClient(**cb_creds)
-    res = client.download_price_data(interval=1, interval_type='d', back_shift=0, num_bars=600)
+
+    res = client.download_price_data(
+        interval=interval, interval_type=interval_type, back_shift=0, num_bars=num_bars,
+    )
+    dates = res.index
+    res = res.reset_index(drop=True)
+    time_series = pd.DataFrame({'time': dates}, index=res.index)
+    # bench_data = scanner.yf_get_stock_data('BITW', days=num_bars, interval=interval_str)
+    # bench_data = bench_data.reset_index()
+    # bench_time_series = pd.DataFrame({'time': bench_data.time}, index=bench_data.index)
+
+    res.to_csv(fr'{base_address}\history.csv')
+    time_series.to_csv(fr'{base_address}\date_time.csv')
+    # bench_data.to_csv(fr'{base_address}\bench.csv')
+    # bench_time_series.to_csv(fr'{base_address}\bench.csv')
+
     print('d')
 
 
+if __name__ == '__main__':
+    download_macro()
+    print('d')
